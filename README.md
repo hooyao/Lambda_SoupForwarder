0d7cc3544417a89a2f9560c5bfb9a5ef2fb99ba0
Yao Hu <huy@tradeshift.com>
2018-12-11 16:19:18 +0800

    init soap forwarder

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Lambda_SoupForwarder
+
+## Setup
+
+```bash
+export AWS_ACCESS_KEY_ID=${your_aws_access_key_id}
+export AWS_SECRET_ACCESS_KEY=${your_aws_secret_key}
+```
+
+**Switch your python env to python 3.6, it doesn't matter how, conda or virtualenv, you pick**
+
+```bash
+npm install # install serverless framework
+sls deploy  # use serverless framework to deploy
+```
+
+if everthing goes as we expected, you will see following output. Of course you need a working AWS account.
+```
+Service Information
+service: stapletest
+stage: dev
+region: ap-northeast-1
+stack: stapletest-dev
+api keys:
+  None
+endpoints:
+  POST - https://xxxxx.execute-api.ap-northeast-1.amazonaws.com/dev/tswsqlforwarder
+functions:
+  hello: stapletest-dev-hello
+layers:
+  None
+```
---
+++ handler.py
@@ -0,0 +1,67 @@
+import xml.etree.ElementTree as ET
+
+import requests
+
+
+def format_babelway_request(soap_body):
+    return "<?xml version='1.0'?>" + \
+           '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + \
+           '  <soap:Body>' + \
+           '    <ns2:postMessage xmlns:ns2="http://xmlns.babelway.com/2007/soapgatewayin">' + \
+           '      <message>' + \
+           '        <xmlPayload>' + \
+           f"         {soap_body}" + \
+           '        </xmlPayload>' + \
+           '      </message>' + \
+           '    </ns2:postMessage>' + \
+           '  </soap:Body>' + \
+           '</soap:Envelope>'
+
+
+def basic_auth_wsdl_forwarder(event, context):
+    body = event['body']
+    auth = event['headers']['Authorization']
+    root = ET.fromstring(body)
+    soap_body = root.find('{http://schemas.xmlsoap.org/soap/envelope/}Body')
+
+    if soap_body:
+        babelway_req_content = format_babelway_request(ET.tostring(soap_body, encoding='unicode', method='xml'))
+        with requests.Session() as s:
+            headers = {'Content-Type': 'application/xml', 'Authorization': auth}
+            babelway_response = s.post(url="http://xxxx.tradeshift.com/ws/SoapIn", data=babelway_req_content,
+                                       headers=headers)
+            req_body = babelway_response.text
+            return {
+                'statusCode': babelway_response.status_code,
+                'body': req_body
+            }
+    else:
+        return {
+            'statusCode': 400,
+            'body': ''
+        }
+
+
+body = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'>" \
+       "  <SOAP:Body>" \
+       "    <SOAP:Fault>" \
+       "      <faultcode>SOAP:Server</faultcode>" \
+       "      <faultstring>Server Error</faultstring>" \
+       "      <detail>" \
+       "        <s:SystemError xmlns:s='http://sap.com/xi/WebService/xi2.0'>" \
+       "          <context>XIAdapter</context>" \
+       "          <code>ADAPTER.JAVA_EXCEPTION</code>" \
+       "          <text>some content</text>" \
+       "        </s:SystemError>" \
+       "      </detail>" \
+       "    </SOAP:Fault>" \
+       "  </SOAP:Body>" \
+       "</SOAP:Envelope>"
+
+auth = "Basic dHJhZGVzaGlmdDp0cmFkZXNoaWZ0"
+
+event = {'body': body, 'headers': {'Authorization': auth}}
+
+response = basic_auth_wsdl_forwarder(event, None)
+
+print(response)
